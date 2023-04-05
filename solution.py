76596688e3570000649a9a461ef634d1abd23601
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2023-04-05 10:51:01 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,16 +4,17 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 303247798 # Ваш chat ID, не меняйте название переменной
+chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     n = len(x)
     sample_mean = np.mean(x)
-    sample_std = np.std(x, ddof=1)  # используем несмещенную оценку стандартного отклонения
+    
+    sample_std = np.std(x, ddof=1)
 
-    t_critical = abs(np.round(norm.t.ppf((1 - p) / 2, n - 1), 4))  # критическое значение t-статистики
+    t_critical = abs(np.round(norm.ppf((1 - p) / 2, n - 1), 4))
 
     left_boundary = round(sample_mean - (t_critical * sample_std / np.sqrt(n)), 4)
     right_boundary = round(sample_mean + (t_critical * sample_std / np.sqrt(n)), 4)
 
-    return (left_boundary, right_boundary)
+    return left_boundary, right_boundary
