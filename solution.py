61a906795a90320805784dc885f1357a4b64f38b
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2023-04-05 10:16:10 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,7 +11,7 @@ def solution(p: float, x: np.array) -> tuple:
     sample_mean = np.mean(x)
     sample_std = np.std(x, ddof=1)  # используем несмещенную оценку стандартного отклонения
 
-    t_critical = abs(np.round(stats.t.ppf((1 - p) / 2, n - 1), 4))  # критическое значение t-статистики
+    t_critical = abs(np.round(norm.t.ppf((1 - p) / 2, n - 1), 4))  # критическое значение t-статистики
 
     left_boundary = round(sample_mean - (t_critical * sample_std / np.sqrt(n)), 4)
     right_boundary = round(sample_mean + (t_critical * sample_std / np.sqrt(n)), 4)
